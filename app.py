e9f55d059271a2e3ea72b15833a7aa50c7952879
mutheswaran <128276598+muthes21@users.noreply.github.com>
2023-03-19 11:04:35 +0000

    merged login and registration and printed the data

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello_user():
-    return render_template('home.html') 
+  if request.method == 'POST':
+    user = request.form['uname']
+    pwd = request.form['psd']
+    print(user, psd)
+  return render_template('home.html')
+
+
+@app.route("/Register")
+def Register():
   
-  
+  return render_template('RegisterHere.html')
+
+
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
